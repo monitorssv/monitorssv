f8ba3937b6048ede6ce3130d4bce5367ef274323
monitorssv <vitalethd@gmail.com>
2024-10-10 21:55:21 +0800

    add balance_delta_test.go

--- eth2/balance_delta_test.go
+++ eth2/balance_delta_test.go
@@ -9,3 +9,14 @@ func TestValidatorMonitor(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestCheckBalanceChange(t *testing.T) {
+	bm := initBeaconMonitor(t)
+	bm.updateBalanceHistory(1, 1, 100)
+	t.Log(bm.validatorBalanceHistory)
+	t.Log(bm.checkBalanceChange(1, true))
+	bm.updateBalanceHistory(1, 2, 101)
+	t.Log(bm.validatorBalanceHistory)
+	bm.updateBalanceHistory(1, 3, 102)
+	t.Log(bm.validatorBalanceHistory)
+}
